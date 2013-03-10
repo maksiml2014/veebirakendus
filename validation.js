ab9e9ca7d0bf09f8af5a7ac30a4ec08b6bb3a1bf
heiran <heikzz@gmail.com>
2013-03-10 23:53:09 +0200

    see krdi asi ei tööta ikka päris nii nagu vaja -.-

---
+++ validation.js
@@ -0,0 +1,28 @@
+﻿function select_radio_button(group_p){
+	document.getElementById(group_p).className='normal_p';
+}
+
+function is_selected(rb_group,lbl_info){
+	var radio_group =document.getElementsByName(rb_group);
+	for(var x=0;x<radio_group.length;x++){
+		if(radio_group[x].checked){
+			document.getElementById(lbl_info).className='normal_p';
+			return 0;
+		}
+	}
+	document.getElementById(lbl_info).className='error_p';
+	return 1;
+}
+function validation(){
+	var error=0;
+	error+=is_selected("area","area_info");
+	error+=is_selected("partei","partei_info");
+	if(error>0){
+		alert("Palun täitke kõik vajalikud väljad!");
+		return false;
+	}
+	else{
+		return true;
+	}
+}	
+
