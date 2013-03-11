6f2a2bfc12e442972f3cf57deecd6daa72ca45f9
Max <maksiml@ut.ee>
2013-03-11 14:12:52 +0200

    parandus

--- search.js
+++ search.js
@@ -19,7 +19,7 @@ function getForm(form) {
 		});
 	}
 	
-	if (piirkond != 0 && partei != 100) {
+	else if (piirkond != 0 && partei != 100) {
 		jQuery('#scriptinput').empty();
 		jQuery.getJSON("findCandidatesByPartyAndRegion.json", function(result) { //json fail
 			jQuery.each(result, function(index, item){  //list candidates
@@ -38,7 +38,7 @@ function getForm(form) {
 		});
 	}
 	
-	if (piirkond !=0){
+	else if (piirkond !=0){
 		jQuery('#scriptinput').empty();
 		jQuery.getJSON("findCandidatesByRegion.json", function(result){
 			jQuery.each(result, function(index, item){
@@ -54,7 +54,7 @@ function getForm(form) {
 		});
 		
 	}
-	if (partei !=0){
+	else if (partei !=100){
 		jQuery('#scriptinput').empty();
 		jQuery.getJSON("findCandidatesByParty.json", function(result){
 			jQuery.each(result, function(index, item){
@@ -71,25 +71,7 @@ function getForm(form) {
 		
 	}
 	
+	else {jQuery('#scriptinput').empty();}
+	
 
-	// alert (name);
-
-	/*
-	 * var e = document.getElementById("partei").value; //alert(e); if (e !=
-	 * 100) {arr.push("party:" + e); nrSearched += 1;}
-	 * 
-	 * 
-	 * var f = document.getElementById("piirkond").value; //alert(f); if (f !=
-	 * 0) {arr.push("region:" + f); nrSearched += 1;}
-	 * 
-	 * //alert(nrSearched);
-	 * 
-	 * 
-	 * if (nrSearched == 1){ } jQuery("#scriptinput").empty();
-	 * jQuery.getJSON("findCandidatesByParty.json", function(result) { var obj =
-	 * jQuery('#scriptinput'); jQuery.each(result, function(index, item) { if
-	 * (index != "id") { jQuery.each(item, function(key, val) { if (key != "id") {
-	 * obj.append(jQuery("<p><br>" + val.region.name + '<br />' +
-	 * val.person.name + "</p>")); } }); } }); });
-	 */
 };
