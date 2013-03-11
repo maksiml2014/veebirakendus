087c856226e572855c65594e23744ade6981bf9b
Max <maksiml@ut.ee>
2013-03-11 13:59:15 +0200

    Json failidest htmli asendamine tehtud. Öösel teen kujunduse tagasi korda

--- search.js
+++ search.js
@@ -1,54 +1,95 @@
-// Load JSON text from server hosted file and return JSON parsed object
-function loadJSON(filePath) {
-  // Load json file;
-  var json = loadTextFileAjaxSync(filePath, "application/json");
-  // Parse json
-  return JSON.parse(optionsText);
-}   
+function getForm(form) {
 
-
-//Load text with Ajax synchronously: takes path to file and optional MIME type
-function loadTextFileAjaxSync(filePath, mimeType)
-{
-  var xmlhttp=new XMLHttpRequest();
-  xmlhttp.open("GET",filePath,false);
-  if (mimeType != null) {
-    if (xmlhttp.overrideMimeType) {
-      xmlhttp.overrideMimeType(mimeType);
-    }
-  }
-  xmlhttp.send();
-  if (xmlhttp.status==200)
-  {
-    return xmlhttp.responseText;
-  }
-  else {
-    // TODO Throw exception
-    return null;
-  }
-}
-
-function searchButtonAction() {//v1.0
-	var name = "lalala";
-	var area = 0;
-	var party = 100;
-	name = document.getElementById("search-box").value;
-	var arealist = document.getElementById("choosearea");
-	area = arealist.options[arealist.selectedIndex].value;
-	var partylist = document.getElementById("chooseparty");
-	party = partylist.options[partylist.selectedIndex].value;
+	var obj = jQuery('#scriptinput');
+	var name = form.searchbox.value;
+	var piirkond = document.getElementById("piirkond").value;
+	var partei = document.getElementById("partei").value;
 	
-	alert (name);
-	alert (area);
-	alert (party);
 	
-	if (JSONObject.name==name){
-		alert ("if found")
-		document.getElementById("candidatename").innerHTML=JSONObject.name;
-		document.getElementById("resultinfo").innerHTML=JSONObject.info;
-		document.getElementById("addinfo").innerHTML=JSONObject.addinfo;
-		document.getElementById("voteid").innerHTML=JSONObject.id;
-		document.getElementById("candidatepic").src=JSONObject.image;
+	if (name != "") {
+		jQuery.getJSON("candidate.json", function(result) {
+
+			jQuery.each(result, function(index, item) {
+				if (index != "id") {
+					jQuery('#scriptinput').empty();
+					obj.append(jQuery("<p><br>" + item.name + '<br /></p>'));
+
+				}
+			});
+		});
+	}
+	
+	if (piirkond != 0 && partei != 100) {
+		jQuery('#scriptinput').empty();
+		jQuery.getJSON("findCandidatesByPartyAndRegion.json", function(result) { //json fail
+			jQuery.each(result, function(index, item){  //list candidates
+				if (index !="id"){
+					jQuery.each(item, function(key, val){ //
+						if (key != "id"){
+							
+							obj.append(jQuery("<p><br>" + val.person.name + '<br /></p>'));
+						}
+					});
+				}
+				
+				
+			});
+
+		});
+	}
+	
+	if (piirkond !=0){
+		jQuery('#scriptinput').empty();
+		jQuery.getJSON("findCandidatesByRegion.json", function(result){
+			jQuery.each(result, function(index, item){
+				if (index != "id"){
+					jQuery.each(item, function(key, val){
+						if (key !="id"){
+							//alert(val.person.name)
+							obj.append(jQuery("<p><br>" + val.person.name + ' ' + val.party.name + '<br /></p>'));
+						}
+					});
+				}
+			});
+		});
 		
 	}
-}
+	if (partei !=0){
+		jQuery('#scriptinput').empty();
+		jQuery.getJSON("findCandidatesByParty.json", function(result){
+			jQuery.each(result, function(index, item){
+				if (index != "id"){
+					jQuery.each(item, function(key, val){
+						if (key !="id"){
+							//alert(val.person.name)
+							obj.append(jQuery("<p><br>" + val.person.name + ' ' + val.region.name + '<br /></p>'));
+						}
+					});
+				}
+			});
+		});
+		
+	}
+	
+
+	// alert (name);
+
+	/*
+	 * var e = document.getElementById("partei").value; //alert(e); if (e !=
+	 * 100) {arr.push("party:" + e); nrSearched += 1;}
+	 * 
+	 * 
+	 * var f = document.getElementById("piirkond").value; //alert(f); if (f !=
+	 * 0) {arr.push("region:" + f); nrSearched += 1;}
+	 * 
+	 * //alert(nrSearched);
+	 * 
+	 * 
+	 * if (nrSearched == 1){ } jQuery("#scriptinput").empty();
+	 * jQuery.getJSON("findCandidatesByParty.json", function(result) { var obj =
+	 * jQuery('#scriptinput'); jQuery.each(result, function(index, item) { if
+	 * (index != "id") { jQuery.each(item, function(key, val) { if (key != "id") {
+	 * obj.append(jQuery("<p><br>" + val.region.name + '<br />' +
+	 * val.person.name + "</p>")); } }); } }); });
+	 */
+};
