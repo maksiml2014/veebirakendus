5f2f8fab55a9cbdafb572f07e82af1642929a942
Max <maksiml@ut.ee>
2013-03-12 17:45:32 +0200

    Kandidaati otsing pooleli

--- search.js
+++ search.js
@@ -1,52 +1,68 @@
 function getForm(form) {
 
-	var obj = jQuery('#scriptinput');
+	
+	var div = jQuery('#found');
+	var found = jQuery('#foundbody');
+	var tablehead = jQuery('#tablehead');
 	var name = form.searchbox.value;
 	var piirkond = document.getElementById("piirkond").value;
 	var partei = document.getElementById("partei").value;
 	
 	
-	if (name != "") {
+	if (name != "" && piirkond==0 && partei == 100) {
+		jQuery(tablehead).empty();
+		jQuery(found).empty();
+		found.append(jQuery("<tr>"));
+		tablehead.append(jQuery("<th>Partei</th><th>Piirkond</th><th>Kandidaat</th>"));
 		jQuery.getJSON("candidate.json", function(result) {
 
 			jQuery.each(result, function(index, item) {
+				
 				if (index != "id") {
-					jQuery('#scriptinput').empty();
-					obj.append(jQuery("<p><br>" + item.name + '<br /></p>'));
+					jQuery(div).empty();
+					div.append(jQuery("<p><br> Kandidaat leitud: </p><br />"));
+					found.append(jQuery("<td>" + item.name + ' </td>'));
 
 				}
 			});
 		});
+		found.append(jQuery("</tr>"));
 	}
 	
-	else if (piirkond != 0 && partei != 100) {
-		jQuery('#scriptinput').empty();
+	else if (piirkond != 0 && partei != 100 && name=="") {
+		jQuery(tablehead).empty();
+		jQuery(found).empty();
+		
+		tablehead.append(jQuery("<th>Kandidaat</th>"));
 		jQuery.getJSON("findCandidatesByPartyAndRegion.json", function(result) { //json fail
 			jQuery.each(result, function(index, item){  //list candidates
 				if (index !="id"){
 					jQuery.each(item, function(key, val){ //
 						if (key != "id"){
-							
-							obj.append(jQuery("<p><br>" + val.person.name + '<br /></p>'));
+							jQuery(div).empty();
+							div.append(jQuery("<p><br> Kandidaat leitud: </p><br />"));
+							found.append(jQuery("<tr><td>" + val.person.name + '</td></tr>'));
 						}
 					});
 				}
-				
-				
 			});
-
 		});
 	}
 	
-	else if (piirkond !=0){
-		jQuery('#scriptinput').empty();
+	else if (piirkond !=0 && name=="" && partei==100){
+		jQuery(tablehead).empty();
+		jQuery(found).empty();
+		
+		tablehead.append(jQuery("<th>Kandidaat</th><th>Partei</th>"));
 		jQuery.getJSON("findCandidatesByRegion.json", function(result){
 			jQuery.each(result, function(index, item){
 				if (index != "id"){
 					jQuery.each(item, function(key, val){
 						if (key !="id"){
+							jQuery(div).empty()
+							div.append(jQuery("<p><br> Kandidaat leitud: </p><br />"));
 							//alert(val.person.name)
-							obj.append(jQuery("<p><br>" + val.person.name + ' ' + val.party.name + '<br /></p>'));
+							found.append(jQuery("<tr><td>" + val.person.name + '</td><td> ' + val.party.name + '</tr></td>'));
 						}
 					});
 				}
@@ -54,7 +70,7 @@ function getForm(form) {
 		});
 		
 	}
-	else if (partei !=100){
+	else if (partei !=100 && name=="" && piirkond==0){
 		jQuery('#scriptinput').empty();
 		jQuery.getJSON("findCandidatesByParty.json", function(result){
 			jQuery.each(result, function(index, item){
@@ -71,7 +87,11 @@ function getForm(form) {
 		
 	}
 	
-	else {jQuery('#scriptinput').empty();}
+	else {jQuery(div).empty();
+	jQuery(tablehead).empty();
+	jQuery(found).empty();
+	div.append(jQuery("<p>Teie päringule ei leitud vastust</p>"))
+			}
 	
 
 };
