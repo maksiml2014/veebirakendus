1d065847dd3a8bd918642a197a77f913b2fe70e1
KPeedosk <KPeedosk@Gmail.com>
2013-03-29 12:36:19 +0200

    tabelid sortable ja load on tab

--- search.js
+++ search.js
@@ -2,18 +2,17 @@ function getForm(form) {
 
 	
 	var div = jQuery('#found');
-	var found = jQuery('#foundbody');
-	var tablehead = jQuery('#tablehead');
+	var table= jQuery('#tablejson')
 	var name = form.searchbox.value;
+	
 	var piirkond = document.getElementById("piirkond").value;
 	var partei = document.getElementById("partei").value;
 	
 	
 	if (name != "" && piirkond==0 && partei == 100) {
-		jQuery(tablehead).empty();
-		jQuery(found).empty();
-		found.append(jQuery("<tr>"));
-		tablehead.append(jQuery("<th>Partei</th><th>Piirkond</th><th>Kandidaat</th>"));
+		jQuery(table).empty();
+		
+		table.append(jQuery("<thead><th>Partei</th><th>Piirkond</th><th>Kandidaat</th><thead><tbody><tr>"));
 		jQuery.getJSON("candidate.json", function(result) {
 
 			jQuery.each(result, function(index, item) {
@@ -21,19 +20,18 @@ function getForm(form) {
 				if (index != "id") {
 					jQuery(div).empty();
 					div.append(jQuery("<p><br> Kandidaat leitud: </p><br />"));
-					found.append(jQuery("<td>" + item.name + ' </td>'));
+					table.append(jQuery("<td>" + item.name + ' </td>'));
 
 				}
 			});
 		});
-		found.append(jQuery("</tr>"));
+		table.append(jQuery("</tr></tbody>"));
 	}
 	
 	else if (piirkond != 0 && partei != 100 && name=="") {
-		jQuery(tablehead).empty();
-		jQuery(found).empty();
+		jQuery(table).empty();
 		
-		tablehead.append(jQuery("<th>Kandidaat</th>"));
+		table.append(jQuery("<thead><th>Kandidaat</th><thead><tbody>"));
 		jQuery.getJSON("findCandidatesByPartyAndRegion.json", function(result) { //json fail
 			jQuery.each(result, function(index, item){  //list candidates
 				if (index !="id"){
@@ -41,19 +39,19 @@ function getForm(form) {
 						if (key != "id"){
 							jQuery(div).empty();
 							div.append(jQuery("<p><br> Kandidaat leitud: </p><br />"));
-							found.append(jQuery("<tr><td>" + val.person.name + '</td></tr>'));
+							table.append(jQuery("<tr><td>" + val.person.name + '</td></tr>'));
 						}
 					});
 				}
 			});
 		});
+		table.append(jQuery("</tbody>"));
 	}
 	
 	else if (piirkond !=0 && name=="" && partei==100){
-		jQuery(tablehead).empty();
-		jQuery(found).empty();
-		
-		tablehead.append(jQuery("<th>Kandidaat</th><th>Partei</th>"));
+		jQuery(table).empty();
+
+		table.append(jQuery("<thead><th>Kandidaat</th><th>Partei</th><thead><tbody>"));
 		jQuery.getJSON("findCandidatesByRegion.json", function(result){
 			jQuery.each(result, function(index, item){
 				if (index != "id"){
@@ -62,19 +60,18 @@ function getForm(form) {
 							jQuery(div).empty()
 							div.append(jQuery("<p><br> Kandidaat leitud: </p><br />"));
 							//alert(val.person.name)
-							found.append(jQuery("<tr><td>" + val.person.name + '</td><td> ' + val.party.name + '</td></tr>'));
+							table.append(jQuery("<tr><td>" + val.person.name + '</td><td> ' + val.party.name + '</td></tr>'));
 						}
 					});
 				}
 			});
 		});
-		
+		table.append(jQuery("</tbody>"));
 	}
 	else if (partei !=100 && name=="" && piirkond==0){
-		jQuery(tablehead).empty();
-		jQuery(found).empty();
+		jQuery(table).empty();
 		
-		tablehead.append(jQuery("<th>Kandidaat</th><th>Piirkond</th>"));
+		table.append(jQuery("<thead><th>Kandidaat</th><th>Piirkond</th></thead><tbody>"));
 		jQuery.getJSON("findCandidatesByParty.json", function(result){
 			jQuery.each(result, function(index, item){
 				if (index != "id"){
@@ -83,23 +80,22 @@ function getForm(form) {
 						div.append(jQuery("<p><br> Kandidaat leitud: </p><br />"));
 						if (key !="id"){
 							//alert(val.person.name)
-							found.append(jQuery("<tr><td>" + val.person.name + '</td><td> ' + val.region.name + '</td></tr>'));
+							table.append(jQuery("<tr><td>" + val.person.name + '</td><td> ' + val.region.name + '</td></tr>'));
 						}
 					});
 				}
 			});
 		});
-		
+		table.append(jQuery("</tbody>"));
 	}
 	
 	
 
 	else if (partei ==100 && name=="" && piirkond==0){
-		jQuery(tablehead).empty();
-		jQuery(found).empty();
 		jQuery(div).empty()
+		jQuery(table).empty()
 		
-		tablehead.append(jQuery("<th>Kandidaat</th><th>Piirkond</th><th>Partei</th>"));
+		table.append(jQuery("<thead><th>Kandidaat</th><th>Piirkond</th><th>Partei</th></thead><tbody>"));
 		div.append(jQuery("<p><br> Kandidaat leitud: </p><br />"));
 		
 		
@@ -111,9 +107,11 @@ function getForm(form) {
 			
 			jQuery.each(data, function(index, item){
 				
-				found.append(jQuery("<tr><td>" + item.name + "</td><td>" + item.region_name + "</td><td>" + item.party_name + "</td></tr>"));
+				table.append(jQuery("<tr><td>" + item.name + "</td><td>" + item.region_name + "</td><td>" + item.party_name + "</td></tr>"));
 			
 			});
+			jQuery.append("</tbody>")
+			sorttable.makeSortable(jQuery(table));
 		});
 		
 		
@@ -121,8 +119,8 @@ function getForm(form) {
 	
 	
 	else {jQuery(div).empty();
-	jQuery(tablehead).empty();
-	jQuery(found).empty();
+	jQuery(table).empty();
+
 	div.append(jQuery("<p>Teie päringule ei leitud vastust</p>"))
 			}
 	
