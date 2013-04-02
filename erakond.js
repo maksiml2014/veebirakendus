e3694f2dd8c3c5d5b56210647831d50eaca26da0
KPeedosk <KPeedosk@Gmail.com>
2013-04-02 18:55:02 +0300

    make it well

--- erakond.js
+++ erakond.js
@@ -1,19 +1,25 @@
 function Stattable(form) {
-
-	var table= jQuery('#tablestat')
-
+	hidelogout();
+	
+	
+	var table= jQuery('#tablestat');
+	var piirkond = document.getElementById("statpiirkond").value;
+	var partei = document.getElementById("statpartei").value;
+	var link="statpartei?statpiirkond=" + piirkond + "&statpartei=" + partei
+	var link2="statcandidate?statpiirkond=" + piirkond + "&statpartei=" + partei
+	
+	
 	jQuery(table).empty()
 	table.append(jQuery("<thead><th>Erakond</th><th>% häältest</th><th>hääli</th></thead><tbody>"));
-		jQuery.getJSON("statpartei", function(data){
+		jQuery.getJSON("statpartei?statpiirkond=0&statpartei=100", function(data){
 		jQuery.each(data, function(index, item){
 			table.append(jQuery("<tr><td>" + item.name + "</td><td>" + item.pc + "</td><td>" + item.votes + "</td></tr>"));
 		});
 		table.append(jQuery("</tr></tbody>"));
 	});	
 		
-	var piirkond = document.getElementById("statpiirkond").value;
-	var partei = document.getElementById("statpartei").value;
-	var link="statpartei?statpiirkond=" + piirkond + "&statpartei=" + partei
+
+	
 	var table2= jQuery('#tablestat2')
 	
 	jQuery(table2).empty()
@@ -30,7 +36,7 @@ function Stattable(form) {
 	var table3= jQuery('#tablestat3')
 	jQuery(table3).empty()		
 	table3.append(jQuery("<thead><th>Kandidaat</th><th>% häältest</th><th>hääli</th></thead><tbody>"));
-	jQuery.getJSON(link, function(data){
+	jQuery.getJSON(link2, function(data){
 		jQuery.each(data, function(index, item){
 			table3.append(jQuery("<tr><td>" + item.name + "</td><td>" + item.pc + "</td><td>" + item.votes + "</td></tr>"));
 		});
---
+++ hidelogoutbutton.js
@@ -0,0 +1,12 @@
+function hidelogout(){
+	document.getElementById("logoutbutton").style.visibility = 'hidden'; 
+}
+function hidelogin(){
+	document.getElementById("loginbutton").style.visibility = 'hidden'; 
+}
+function showlogout(){
+	document.getElementById("logoutbutton").style.visibility = 'visible'; 
+}
+function showlogin(){
+	document.getElementById("loginbutton").style.visibility = 'visible'; 
+}
