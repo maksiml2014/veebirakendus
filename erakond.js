2e1e49dc02c4d35f67f4200e3993b316e29c07d2
KPeedosk <KPeedosk@Gmail.com>
2013-04-03 01:55:37 +0300

    töötav graafik ka.

--- erakond.js
+++ erakond.js
@@ -6,17 +6,21 @@ function Stattable(form) {
 	var partei = document.getElementById("statpartei").value;
 	var link="statpartei?statpiirkond=" + piirkond + "&statpartei=" + partei
 	var link2="statcandidate?statpiirkond=" + piirkond + "&statpartei=" + partei
-	
+	var andmed= new Array();
+	var andmed2= new Array();
 	
 	jQuery(table).empty()
 	table.append(jQuery("<thead><th>Erakond</th><th>% häältest</th><th>hääli</th></thead><tbody>"));
 		jQuery.getJSON("statpartei?statpiirkond=0&statpartei=100", function(data){
 		jQuery.each(data, function(index, item){
 			table.append(jQuery("<tr><td>" + item.name + "</td><td>" + item.pc + "</td><td>" + item.votes + "</td></tr>"));
+			andmed.push([item.name, item.votes]);
 		});
 		table.append(jQuery("</tr></tbody>"));
+		console.log(andmed);
+		drawChart(andmed,'map');	
 	});	
-		
+	
 
 	
 	var table2= jQuery('#tablestat2')
@@ -26,8 +30,11 @@ function Stattable(form) {
 	jQuery.getJSON(link, function(data){
 		jQuery.each(data, function(index, item){
 				table2.append(jQuery("<tr><td>" + item.name + "</td><td>" + item.pc + "</td><td>" + item.votes + "</td></tr>"));
+				andmed2.push([item.name, item.votes]);
 		});
 		table2.append(jQuery("</tr></tbody>"));
+		console.log(andmed2);
+		drawChart(andmed2,'chart_div');
 	});
 		
 		
@@ -41,7 +48,5 @@ function Stattable(form) {
 		});
 	table3.append(jQuery("</tr></tbody>"));
 	});
-	
-	
-}
 
+}
--- googlechart.js
+++ googlechart.js
@@ -8,40 +8,25 @@
       // Callback that creates and populates a data table,
       // instantiates the pie chart, passes in the data and
       // draws it.
-      function drawChart() {
+      function drawChart(andmed,div) {
     	  
-    	var piirkond = document.getElementById("statpiirkond").value;
-    	var partei = document.getElementById("statpartei").value;
-    	var link="statpartei?statpiirkond=" + piirkond + "&statpartei=" + partei
-        clearChart();
-    	var list;
-    	// Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Hääli');
-  //      data.addRows([
-    //      ['Mushrooms', 3],
-      //    ['Onions', 1],
-   //       ['Olives', 1],
-   //       ['Zucchini', 1],
-   //       ['Pepperoni', 2]
-  //      ]);
-        
-        jQuery.getJSON(link, function(data){
-    		jQuery.each(data, function(index, item){
-    			list.append(jQuery("['" + item.partei +"', " + item.votes + "],"))
-    		});
-    		finallist=list.substring(0, str.length-1);
-    		data.addRows([finallist]);
-        }	
     		
-        // Set chart options
-        var options = {'title':'Hääli',
-                       'width':$("#itemL").width(),
-                       'height':300,
-                       'backgroundColor': "#6fa8dc"};
+    		// Create the data table.
+    	    var data = new google.visualization.DataTable();
+    	    data.addColumn('string', 'name');
+    	    data.addColumn('number', 'votes');
+    	    for (var i=0; i<andmed.length; i++){
+    	    	data.addRow(andmed[i]);
+    	    }
+    	    
+    	    // Set chart options
+    	    var options = {'title':'Hääli',
+    	                   'width':$("#itemL").width(),
+    	                   'height':300,
+    	                   'backgroundColor': "#6fa8dc"};
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+    	    // Instantiate and draw our chart, passing in some options.
+    	    var chart = new google.visualization.PieChart(document.getElementById(div));
+    	    chart.draw(data, options);
+    	  }
+
