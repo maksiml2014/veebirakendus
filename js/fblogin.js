747282934542502de0ea1afaf064ac9d89c4a991
KPeedosk <KPeedosk@Gmail.com>
2013-04-14 19:51:51 +0300

    login and stuff

--- js/fblogin.js
+++ js/fblogin.js
@@ -1,7 +1,73 @@
-﻿(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=528306117210739";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+﻿  var button;
+  var userInfo;
+  
+  window.fbAsyncInit = function() {
+      FB.init({ appId: '528306117210739', //change the appId to your appId
+          status: true,
+          cookie: true,
+          xfbml: true,
+          oauth: true});
+
+     
+     function updateButton(response) {
+          button = document.getElementById('loginbutton');
+          userInfo = document.getElementById('user-info');
+          
+          if (response.authResponse) {
+              //user is already logged in and connected
+              FB.api('/me', function(info) {
+                  login(response, info);
+              });
+              
+              button.onclick = function() {
+                  FB.logout(function(response) {
+                      logout(response);
+                  });
+              };
+          } else {
+              //user is not connected to your app or logged out
+              button.innerHTML = 'Login';
+              button.onclick = function() {
+                  FB.login(function(response) {
+                      if (response.authResponse) {
+                          FB.api('/me', function(info) {
+                              login(response, info);
+                          });	
+                      } else {
+                          //user cancelled login or did not grant authorization
+                      }
+                  }, {scope:'email,user_birthday'});
+              }
+          }
+      }
+      
+      // run once with current status and whenever the status changes
+      FB.getLoginStatus(updateButton);
+      FB.Event.subscribe('auth.statusChange', updateButton);
+  };
+  (function() {
+      var e = document.createElement('script'); e.async = true;
+      e.src = document.location.protocol
+          + '//connect.facebook.net/en_US/all.js';
+      document.getElementById('fb-root').appendChild(e);
+  }());
+  
+  
+  function login(response, info){
+      if (response.authResponse) {
+          var accessToken = response.authResponse.accessToken;
+          
+          userInfo.innerHTML = '<img src="https://graph.facebook.com/' + info.id + '/picture">' + info.name;
+          button.innerHTML = 'Logout';
+          document.getElementById('other').style.display = "block";
+      }
+  }
+
+  function logout(response){
+      userInfo.innerHTML = "";
+      document.getElementById('debug').innerHTML = "";
+      document.getElementById('other').style.display = "none";
+  }
+
+
+  
